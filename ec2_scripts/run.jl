3dc4433167b4bd1e430b2d4ec70e464333d63a02
Chris Coey <coey@mit.edu>
2016-12-05 16:36:16 -0500

    updated run scripts

--- ec2_scripts/run.jl
+++ ec2_scripts/run.jl
@@ -1,15 +1,15 @@
 using ConicBenchmarkUtilities
 using Pajarito
-using ECOS, ConicIP
+# using ECOS, ConicIP
 using Mosek
-using Gurobi
+# using Gurobi
 using Cbc
-using SCIP
-using ConicNonlinearBridge
-using AmplNLWriter
+# using SCIP
+# using ConicNonlinearBridge
+# using AmplNLWriter
 
 
-function solveprint(fd, instance, solver)
+function solveprint(instance, solver)
     # Convert from cbf to our conic format
     (c, A, b, con_cones, var_cones, vartypes, sense, objoffset) = cbftompb(instance)
     if sense == :Max
@@ -48,12 +48,12 @@ function solveprint(fd, instance, solver)
         objbound += objoffset
     end
 
-    println(fd, "#STATUS# $status")
-    println(fd, "#OBJVAL# $objval")
-    println(fd, "#OBJBOUND# $objbound")
-    println(fd, "#TIMESOLVER# $timesolver")
-    println(fd, "#TIMEALL# $timeall")
-    println(fd, "#SOLUTION# $x")
+    println("#STATUS# $status")
+    println("#OBJVAL# $objval")
+    println("#OBJBOUND# $objbound")
+    println("#TIMESOLVER# $timesolver")
+    println("#TIMEALL# $timeall")
+    println("#SOLUTION# $x")
 end
 
 
@@ -81,37 +81,37 @@ function getsolver(solvername, tlim, logl, rgap)
         "PAJ_SUBOPT-8-10_CBC_MOSEK" =>
         PajaritoSolver(mip_solver=CbcSolver(logLevel=0, ratioGap=0., seconds=tlim), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap, mip_subopt_count=8, mip_subopt_solver=CbcSolver(logLevel=0, ratioGap=0.1, seconds=120)),
 
-        # OPEN-SOURCE
-        "PAJ_CBC_ECOS" =>
-        PajaritoSolver(mip_solver=CbcSolver(logLevel=0, ratioGap=0., seconds=tlim), cont_solver=ECOSSolver(verbose=false), log_level=logl, timeout=tlim, rel_gap=rgap),
-        "PAJ_CBC_CIP" =>
-        PajaritoSolver(mip_solver=CbcSolver(logLevel=0, ratioGap=0., seconds=tlim), cont_solver=ConicIPSolver(verbose=false), log_level=logl, timeout=tlim, rel_gap=rgap),
-        "BONMIN_OA" =>
-        ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-OA\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=false),
-        "BONMIN_OADIS" =>
-        ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-OA\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=true),
-        "BONMIN_BB" =>
-        ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-BB\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=false),
-
-        # NON-COMMERCIAL
-        "PAJ_SCIP_MOSEK" =>
-        PajaritoSolver(mip_solver=SCIPSolver("display/verblevel", 0, "limits/gap", 0.0, "limits/time", tlim), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap),
-        "PAJ_MSD_SCIP_MOSEK" =>
-        PajaritoSolver(mip_solver=SCIPSolver("display/verblevel", 0, "limits/gap", rgap, "limits/time", tlim), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap, mip_solver_drives=true),
-        "SCIP_MISOCP" =>
-        ConicNLPWrapper(nlp_solver=SCIPSolver("display/verblevel", 1, "limits/gap", rgap, "limits/time", tlim), soc_as_quadratic=true, disaggregate_soc=false),
-        "SCIP_MISOCPDIS" =>
-        ConicNLPWrapper(nlp_solver=SCIPSolver("display/verblevel", 1, "limits/gap", rgap, "limits/time", tlim), soc_as_quadratic=true, disaggregate_soc=true),
-
-        # COMMERCIAL
-        "PAJ_GUROBI_MOSEK" =>
-        PajaritoSolver(mip_solver=GurobiSolver(OutputFlag=0, Threads=1, TimeLimit=tlim, MIPGap=0.), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap),
-        "PAJ_MSD_GUROBI_MOSEK" =>
-        PajaritoSolver(mip_solver=GurobiSolver(OutputFlag=0, Threads=1, TimeLimit=tlim, MIPGap=rgap), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap, mip_solver_drives=true),
-        "GUROBI_MISOCP" =>
-        GurobiSolver(OutputFlag=1, Threads=1, TimeLimit=tlim, MIPGap=rgap),
-        "MOSEK_MISOCP" =>
-        MosekSolver(LOG=1, NUM_THREADS=1, OPTIMIZER_MAX_TIME=tlim, MIO_TOL_REL_GAP=rgap),
+        # # OPEN-SOURCE
+        # "PAJ_CBC_ECOS" =>
+        # PajaritoSolver(mip_solver=CbcSolver(logLevel=0, ratioGap=0., seconds=tlim), cont_solver=ECOSSolver(verbose=false), log_level=logl, timeout=tlim, rel_gap=rgap),
+        # "PAJ_CBC_CIP" =>
+        # PajaritoSolver(mip_solver=CbcSolver(logLevel=0, ratioGap=0., seconds=tlim), cont_solver=ConicIPSolver(verbose=false), log_level=logl, timeout=tlim, rel_gap=rgap),
+        # "BONMIN_OA" =>
+        # ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-OA\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=false),
+        # "BONMIN_OADIS" =>
+        # ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-OA\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=true),
+        # "BONMIN_BB" =>
+        # ConicNLPWrapper(nlp_solver=BonminNLSolver(["bonmin.algorithm=\"B-BB\"", "bonmin.time_limit=$tlim", "halt_on_ampl_error=yes", "bonmin.allowable_fraction_gap=$rgap", "bonmin.oa_log_level=1", "bonmin.bb_log_level=1"]), disaggregate_soc=false),
+        #
+        # # NON-COMMERCIAL
+        # "PAJ_SCIP_MOSEK" =>
+        # PajaritoSolver(mip_solver=SCIPSolver("display/verblevel", 0, "limits/gap", 0.0, "limits/time", tlim), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap),
+        # "PAJ_MSD_SCIP_MOSEK" =>
+        # PajaritoSolver(mip_solver=SCIPSolver("display/verblevel", 0, "limits/gap", rgap, "limits/time", tlim), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap, mip_solver_drives=true),
+        # "SCIP_MISOCP" =>
+        # ConicNLPWrapper(nlp_solver=SCIPSolver("display/verblevel", 1, "limits/gap", rgap, "limits/time", tlim), soc_as_quadratic=true, disaggregate_soc=false),
+        # "SCIP_MISOCPDIS" =>
+        # ConicNLPWrapper(nlp_solver=SCIPSolver("display/verblevel", 1, "limits/gap", rgap, "limits/time", tlim), soc_as_quadratic=true, disaggregate_soc=true),
+        #
+        # # COMMERCIAL
+        # "PAJ_GUROBI_MOSEK" =>
+        # PajaritoSolver(mip_solver=GurobiSolver(OutputFlag=0, Threads=1, TimeLimit=tlim, MIPGap=0.), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap),
+        # "PAJ_MSD_GUROBI_MOSEK" =>
+        # PajaritoSolver(mip_solver=GurobiSolver(OutputFlag=0, Threads=1, TimeLimit=tlim, MIPGap=rgap), cont_solver=MosekSolver(LOG=0, NUM_THREADS=1, OPTIMIZER_MAX_TIME=120.), log_level=logl, timeout=tlim, rel_gap=rgap, mip_solver_drives=true),
+        # "GUROBI_MISOCP" =>
+        # GurobiSolver(OutputFlag=1, Threads=1, TimeLimit=tlim, MIPGap=rgap),
+        # "MOSEK_MISOCP" =>
+        # MosekSolver(LOG=1, NUM_THREADS=1, OPTIMIZER_MAX_TIME=tlim, MIO_TOL_REL_GAP=rgap),
     )
 
     return deepcopy(solvermap[solvername])
@@ -120,8 +120,7 @@ end
 
 
 # main script
-# usage: run.jl [-stdout] SOLVERNAME TIMELIMIT DATAFOLDER INSTANCENAMES...
-# or   : run.jl [-stdout] -instfile SOLVERNAME TIMELIMIT DATAFOLDER INSTANCENAMESFILE
+# usage: run.jl SOLVERNAME TIMELIMIT DATAFOLDER INSTANCENAME
 
 @assert length(ARGS) >= 3
 
@@ -133,93 +132,39 @@ end
 logl = 2
 rgap = 1e-5
 
-# if ARGS[1] == "-stdout"
-#     nooutfile = true
-#     shift!(ARGS)
-# else
-#     nooutfile = false
-# end
-#
-# if ARGS[1] == "-instfile"
-#     useinstfile = true
-#     shift!(ARGS)
-#     instfile = ARGS[4]
-#     @assert endswith(instfile, ".txt")
-# else
-#     useinstfile = false
-# end
-
 solvername = ARGS[1]
 tlim = parse(Float64, ARGS[2])
 datafolder = ARGS[3]
 
-# if !nooutfile
-#     fdmeta = open("output/$solvername.$tlim.txt", "w")
-#     redirect_stdout(fdmeta)
-#     redirect_stderr(fdmeta)
-# end
-
-#
-# println("#SOLVER# $solvername")
-# println("#TIMELIMIT# $tlim")
-
 # Force Pajarito to compile on a small instance for the solver to avoid measuring compilation time, keep quiet
 if startswith(solvername, "PAJ")
-    instance = readcbfdata(joinpath(datafolder, "estein", "estein4_A.cbf.gz"))
-    solver = getsolver(solvername, 20., 0, rgap)
-    solveprint(STDOUT, instance, solver)
+    TT = STDOUT
+    open("/dev/null", "w") do fd
+        redirect_stdout(fd)
+        instance = readcbfdata(joinpath(datafolder, "estein", "estein4_A.cbf.gz"))
+        solver = getsolver(solvername, 20., 0, rgap)
+        solveprint(instance, solver)
+    end
+    redirect_stdout(TT)
 end
-
-# if useinstfile
-#     # Get instance names to run from instance set file
-#     println("#INSTANCESET# $(basename(instfile))")
-#     instancelist = readdlm(instfile)
-# else
-    # Get instance names to run from args
-    # instancelist = ARGS[4:end]
-# end
-
-# # Print names of result files
-# println("#INSTANCES#")
-# for instancename in instancelist
-#     println("$instancename")
-# end
-# println()
-
-# Start instances in instance list
-# for instancename in instancelist
 
 instancename = ARGS[4]
 
 # Interpret instance data as cbf
 instance = readcbfdata(joinpath(datafolder, instancename))
 
-# Redirect output to a file: SOLVER.INSTANCE.txt
-shortname = chomp(split(instancename, "/")[2])
-filename = "output/$solvername.$shortname.txt"
-# println("\nWriting to $filename...")
+solver = getsolver(solvername, tlim, logl, rgap)
 
-# if !nooutfile
-#     println("\nWriting to $filename...")
-#     TT = STDOUT
-#     TT2 = STDERR
-# end
+println("#SOLVERNAME# $solvername")
+println("#SOLVER# $solver")
+println("#FILENAME# $(basename(instancename))")
+println("#INSTANCE# $(instance.name)")
+println("#TIMELIMIT# $tlim")
 
-open(filename, "w") do fd
-    solver = getsolver(solvername, tlim, logl, rgap)
-    flush(fd)
-    println(fd, "#SOLVERNAME# $solvername")
-    println(fd, "#SOLVER# $solver")
-    println(fd, "#FILENAME# $(basename(instancename))")
-    println(fd, "#INSTANCE# $(instance.name)")
-    println(fd, "#TIMELIMIT# $tlim")
-    flush(fd)
-
-    # Attempt to solve, print solve info
-    timesolve = time()
-    try
-        solveprint(fd, instance, solver)
-    catch e
-        println(fd, "#ERROR# $e")
-    end
+# Attempt to solve, print solve info
+timesolve = time()
+try
+    solveprint(instance, solver)
+catch e
+    println("#ERROR# $e")
 end
--- ec2_scripts/runmeta.jl
+++ ec2_scripts/runmeta.jl
@@ -9,7 +9,7 @@ datafolder = ARGS[4]
 instfile = ARGS[5]
 
 # Print info and all instances in the set to a META file for the solver
-fdmeta = open("output/META.$solvername.txt", "w")
+fdmeta = open("output/META.$solvername.$(split(basename(instfile),'.')[1]).txt", "w")
 println(fdmeta, "#SOLVER# $solvername")
 println(fdmeta, "#TIMELIMIT# $tlim")
 println(fdmeta, "#MEMLIMIT# $mlim")
@@ -26,21 +26,30 @@ flush(fdmeta)
 # This only works on Linux because we check the memory use with Linux commands
 for instancename in instancelist
     shortname = chomp(split(instancename, "/")[2])
+
     println(fdmeta, "\nstarting instance $shortname...")
+
+    filename = "output/$solvername.$shortname.txt"
 
     # Try to start a process to run the current instance on
     # If this fails, it won't affect running future instances
     try
-        process = spawn(`julia run.jl $solvername $tlim $datafolder $instancename`)
+        process = spawn(pipeline(`julia run.jl $solvername $tlim $datafolder $instancename`, stdout=filename, stderr=filename, append=true))
+
         t = time()
         sleep(60.0)
+
         pid = parse(Int, chomp(readline(open("mypid", "r"))))
 
         while process_running(process)
-            if (time() - t) > (tlim + 60*5)
+            if (time() - t) > (tlim + 60*5.0)
                 # Kill if time limit exceeded (some solvers don't respect time limits)
                 kill(process)
-                println(fdmeta, "killed by time limit")
+                sleep(1.0)
+                println(fdmeta, "killed by time limit\n")
+                open(filename, "a") do fd
+                    println(fd, "#STATUS# KilledTime")
+                end
             else
                 # Try to kill if memory limit exceeded (some solvers use too much memory and fail)
                 # If this try fails, process has already stopped
@@ -48,7 +57,11 @@ for instancename in instancelist
                     memuse = parse(Int, split(readstring(pipeline(`cat /proc/$pid/status`,`grep RSS`)))[2])
                     if memuse > mlim
                         kill(process)
-                        println(fdmeta, "killed by memory limit")
+                        sleep(1.0)
+                        println(fdmeta, "killed by memory limit\n")
+                        open(filename, "a") do fd
+                            println(fd, "#STATUS# KilledMemory")
+                        end
                     end
                 end
             end
