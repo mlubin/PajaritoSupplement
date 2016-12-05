3ce2393b92359de84d928d53fc3f8b90e23c66a3
Chris Coey <coey@mit.edu>
2016-12-04 21:37:00 -0500

    commented runmeta.jl, fixed issue with failing to kill process

--- ec2_scripts/runmeta.jl
+++ ec2_scripts/runmeta.jl
@@ -8,6 +8,7 @@ mlim = parse(Int, ARGS[3])
 datafolder = ARGS[4]
 instfile = ARGS[5]
 
+# Print info and all instances in the set to a META file for the solver
 fdmeta = open("output/META.$solvername.txt", "w")
 println(fdmeta, "#SOLVER# $solvername")
 println(fdmeta, "#TIMELIMIT# $tlim")
@@ -21,10 +22,14 @@ end
 println(fdmeta)
 flush(fdmeta)
 
+# Run each instance one by one
+# This only works on Linux because we check the memory use with Linux commands
 for instancename in instancelist
     shortname = chomp(split(instancename, "/")[2])
     println(fdmeta, "\nstarting instance $shortname...")
 
+    # Try to start a process to run the current instance on
+    # If this fails, it won't affect running future instances
     try
         process = spawn(`julia run.jl $solvername $tlim $datafolder $instancename`)
         t = time()
@@ -33,13 +38,18 @@ for instancename in instancelist
 
         while process_running(process)
             if (time() - t) > (tlim + 60*5)
+                # Kill if time limit exceeded (some solvers don't respect time limits)
                 kill(process)
                 println(fdmeta, "killed by time limit")
             else
-                memuse = parse(Int, split(readstring(pipeline(`cat /proc/$pid/status`,`grep RSS`)))[2])
-                if memuse > mlim
-                    kill(process)
-                    println(fdmeta, "killed by memory limit")
+                # Try to kill if memory limit exceeded (some solvers use too much memory and fail)
+                # If this try fails, process has already stopped
+                try
+                    memuse = parse(Int, split(readstring(pipeline(`cat /proc/$pid/status`,`grep RSS`)))[2])
+                    if memuse > mlim
+                        kill(process)
+                        println(fdmeta, "killed by memory limit")
+                    end
                 end
             end
             sleep(1.0)
