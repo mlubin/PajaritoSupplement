7dcd83cabc0227d705ceb13b900d9bace30fbf5f
Chris Coey <coey@mit.edu>
2017-02-06 20:30:50 -0500

    reran process on noslack

--- scripts/process.jl
+++ scripts/process.jl
@@ -30,9 +30,9 @@ function violation_cone(subvec,cone)
     elseif cone == :NonPos
         return :Linear, maximum(max.(subvec, 0.))
     elseif cone == :SOC
-        return :SOC, max(0., sumabs2(subvec[2:end]) - subvec[1]^2)
+        return :SOC, max(0., vecnorm(subvec[2:end]) - subvec[1])
     elseif cone == :SOCRotated
-        return :SOCRotated, max(0., sumabs2(subvec[3:end]) - 2*subvec[1]*subvec[2])
+        return :SOCRotated, max(0., vecnorm(subvec[3:end]) - sqrt(2*subvec[1]*subvec[2]))
     elseif cone == :ExpPrimal
         error("Unexpected expcone")
         return :Exp, max(subvec[2]*exp(subvec[1]/subvec[2]) - subvec[3],0)
