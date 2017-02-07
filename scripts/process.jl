ce087c6ad16b824134b86205f10a995fa806fc14
Chris C <coey.chris@gmail.com>
2017-02-06 20:45:04 -0500

    calc socrot after convert to soc

--- scripts/process.jl
+++ scripts/process.jl
@@ -32,7 +32,8 @@ function violation_cone(subvec,cone)
     elseif cone == :SOC
         return :SOC, max(0., vecnorm(subvec[2:end]) - subvec[1])
     elseif cone == :SOCRotated
-        return :SOCRotated, max(0., vecnorm(subvec[3:end]) - sqrt(2*subvec[1]*subvec[2]))
+        # (y,z,x) in RSOC <=> (y+z,-y+z,sqrt2*x) in SOC, y >= 0, z >= 0
+        return :SOCRotated, max(0., sqrt((subvec[1] - subvec[2])^2 + 2. * sumabs2(subvec[3:end])) - (subvec[1] + subvec[2]))
     elseif cone == :ExpPrimal
         error("Unexpected expcone")
         return :Exp, max(subvec[2]*exp(subvec[1]/subvec[2]) - subvec[3],0)
@@ -46,7 +47,7 @@ end
 
 function compute_violations(dat, solution)
     c, A, b, con_cones, var_cones, vartypes, sense, objoffset = cbftompb(dat)
-    
+
     # will be fixed soon, should really be equal
     #if length(solution) > length(c)
     #    println("Solution is too long")
