3b8b1de894d0be06f4fe19e27cad5b5ecd1a0189
Chris Coey <coey.chris@gmail.com>
2017-07-09 13:49:09 -0400

    run cont tests for only 5min

--- bashrunport.sh
+++ bashrunport.sh
@@ -11,14 +11,14 @@
 #/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl Port_SCS_nowarm 3600 50000000 instancedata/portcbf/socexppsd/ instancesets/port_socexppsd.txt
 
 
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl MOSEK 3600 50000000 continstancedata/soc/ instancesets/contport_soc.txt
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl ECOS 3600 50000000 continstancedata/soc/ instancesets/contport_soc.txt
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 3600 50000000 continstancedata/soc/ instancesets/contport_soc.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl MOSEK 300 50000000 continstancedata/soc/ instancesets/contport_soc.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl ECOS 300 50000000 continstancedata/soc/ instancesets/contport_soc.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 300 50000000 continstancedata/soc/ instancesets/contport_soc.txt
 
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl MOSEK 3600 50000000 continstancedata/socpsd/ instancesets/contport_socpsd.txt
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 3600 50000000 continstancedata/socpsd/ instancesets/contport_socpsd.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl MOSEK 300 50000000 continstancedata/socpsd/ instancesets/contport_socpsd.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 300 50000000 continstancedata/socpsd/ instancesets/contport_socpsd.txt
 
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl ECOS 3600 50000000 continstancedata/socexp/ instancesets/contport_socexp.txt
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 3600 50000000 continstancedata/socexp/ instancesets/contport_socexp.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl ECOS 300 50000000 continstancedata/socexp/ instancesets/contport_socexp.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 300 50000000 continstancedata/socexp/ instancesets/contport_socexp.txt
 
-/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 3600 50000000 continstancedata/socexppsd/ instancesets/contport_socexppsd.txt
+/home/coey/julia-903644385b/bin/julia scripts/runmeta.jl SCS 300 50000000 continstancedata/socexppsd/ instancesets/contport_socexppsd.txt
